8a562208f49f4e9793e2ef3ff33ac26ee1274225
usausa <machi.pon@gmail.com>
2020-01-14 23:45:57 +0900

    Update test

--- Smart.Text.Japanese.Tests/Text/Japanese/SjisEncodingTest.cs
+++ Smart.Text.Japanese.Tests/Text/Japanese/SjisEncodingTest.cs
@@ -6,55 +6,20 @@ namespace Smart.Text.Japanese
 
     public class SjisEncodingTest
     {
-        private static readonly Encoding Enc = SjisEncoding.Instance;
-
         public SjisEncodingTest()
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
         }
 
         [Fact]
-        public void PaddingLeft()
+        public void GetLimitString()
         {
-            Assert.Equal(Enc.GetBytes("あ "), Enc.GetFixedBytes("あ", 3, FixedAlignment.Left));
-            Assert.Equal(Enc.GetBytes("あA "), Enc.GetFixedBytes("あA", 4, FixedAlignment.Left));
-            Assert.Equal(Enc.GetBytes("Aあ "), Enc.GetFixedBytes("Aあ", 4, FixedAlignment.Left));
-        }
-
-        [Fact]
-        public void PaddingRight()
-        {
-            Assert.Equal(Enc.GetBytes(" あ"), Enc.GetFixedBytes("あ", 3, FixedAlignment.Right));
-            Assert.Equal(Enc.GetBytes(" あA"), Enc.GetFixedBytes("あA", 4, FixedAlignment.Right));
-            Assert.Equal(Enc.GetBytes(" Aあ"), Enc.GetFixedBytes("Aあ", 4, FixedAlignment.Right));
-        }
-
-        [Fact]
-        public void PaddingCenter()
-        {
-            Assert.Equal(Enc.GetBytes(" あ "), Enc.GetFixedBytes("あ", 4, FixedAlignment.Center));
-            Assert.Equal(Enc.GetBytes(" あ  "), Enc.GetFixedBytes("あ", 5, FixedAlignment.Center));
-            Assert.Equal(Enc.GetBytes("  あ  "), Enc.GetFixedBytes("あ", 6, FixedAlignment.Center));
-            Assert.Equal(Enc.GetBytes("あA "), Enc.GetFixedBytes("あA", 4, FixedAlignment.Center));
-            Assert.Equal(Enc.GetBytes(" あA "), Enc.GetFixedBytes("あA", 5, FixedAlignment.Center));
-            Assert.Equal(Enc.GetBytes(" あA  "), Enc.GetFixedBytes("あA", 6, FixedAlignment.Center));
-            Assert.Equal(Enc.GetBytes("Aあ "), Enc.GetFixedBytes("Aあ", 4, FixedAlignment.Center));
-            Assert.Equal(Enc.GetBytes(" Aあ "), Enc.GetFixedBytes("Aあ", 5, FixedAlignment.Center));
-            Assert.Equal(Enc.GetBytes(" Aあ  "), Enc.GetFixedBytes("Aあ", 6, FixedAlignment.Center));
-        }
-
-        [Fact]
-        public void Fill()
-        {
-            Assert.Equal(Enc.GetBytes("A "), Enc.GetFixedBytes("A", 2, FixedAlignment.Left));
-            Assert.Equal(Enc.GetBytes("A  "), Enc.GetFixedBytes("A", 3, FixedAlignment.Left));
-            Assert.Equal(Enc.GetBytes("A   "), Enc.GetFixedBytes("A", 4, FixedAlignment.Left));
-            Assert.Equal(Enc.GetBytes("A    "), Enc.GetFixedBytes("A", 5, FixedAlignment.Left));
-            Assert.Equal(Enc.GetBytes("A     "), Enc.GetFixedBytes("A", 6, FixedAlignment.Left));
-            Assert.Equal(Enc.GetBytes("A      "), Enc.GetFixedBytes("A", 7, FixedAlignment.Left));
-            Assert.Equal(Enc.GetBytes("A       "), Enc.GetFixedBytes("A", 8, FixedAlignment.Left));
-            Assert.Equal(Enc.GetBytes("A        "), Enc.GetFixedBytes("A", 9, FixedAlignment.Left));
-            Assert.Equal(Enc.GetBytes("A         "), Enc.GetFixedBytes("A", 10, FixedAlignment.Left));
+            Assert.Equal(string.Empty, SjisEncoding.GetLimitString("あAあAあA", 0, 1));
+            Assert.Equal("あ", SjisEncoding.GetLimitString("あAあAあA", 0, 2));
+            Assert.Equal("あA", SjisEncoding.GetLimitString("あAあAあA", 0, 3));
+            Assert.Equal("あA", SjisEncoding.GetLimitString("あAあAあA", 0, 4));
+            Assert.Equal("あAあ", SjisEncoding.GetLimitString("あAあAあA", 0, 5));
+            Assert.Equal("あAあA", SjisEncoding.GetLimitString("あAあAあA", 0, 6));
         }
     }
 }
